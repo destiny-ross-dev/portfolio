d3216def6491c9e018db5909df26b5a113bef694
Destiny Ross <destinyross.dev@gmail.com>
2019-08-10 17:18:48 -0400

    Updates

---
+++ precache-manifest.6a2a0f3f58c041574e0110d6deb74268.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb4f1e6f1785a77aa84a0f218e52970",
+    "url": "/build/static/index.html"
+  },
+  {
+    "revision": "86b417ddd3259a84f1ff",
+    "url": "/build/static/static/css/main.9ef86bf8.chunk.css"
+  },
+  {
+    "revision": "f523eb80486613b5c89c",
+    "url": "/build/static/static/js/2.356a0e44.chunk.js"
+  },
+  {
+    "revision": "86b417ddd3259a84f1ff",
+    "url": "/build/static/static/js/main.0643ed49.chunk.js"
+  },
+  {
+    "revision": "76535ab8368531adadf1",
+    "url": "/build/static/static/js/runtime~main.dec60502.js"
+  },
+  {
+    "revision": "bad5a5eb10c574aca2f7e86f877b846e",
+    "url": "/build/static/static/media/coven-ss.bad5a5eb.png"
+  },
+  {
+    "revision": "9d4b97d00a2f4eeb957ff5e537582bfa",
+    "url": "/build/static/static/media/crwn-ss.9d4b97d0.png"
+  },
+  {
+    "revision": "13db6bf9c71585a1d0b6834a99035483",
+    "url": "/build/static/static/media/hacker-news-ss.13db6bf9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d40cd5ae57a42b34558b0409ecae5f9a",
+    "url": "/build/static/static/media/react-graphql-apollo.d40cd5ae.md"
+  },
+  {
+    "revision": "d40cd5ae57a42b34558b0409ecae5f9a",
+    "url": "/build/static/static/media/react-redux-hooks.d40cd5ae.md"
+  },
+  {
+    "revision": "672fb03d4bea065fef2c8cef92ab59f8",
+    "url": "/build/static/static/media/resume.672fb03d.pdf"
+  }
+]);
